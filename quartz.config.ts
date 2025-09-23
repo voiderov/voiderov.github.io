d9b9d3ab30913e2e65558bb9ac302f66d8207500
erovadhi <igdeerovadhi@gmail.com>
2025-09-23 21:02:26 +0800

    Quartz sync: Sep 23, 2025, 9:02 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Erov Dossiers",
+    pageTitleSuffix: "Welcome my friend",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
